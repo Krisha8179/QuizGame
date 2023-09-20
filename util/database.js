a4bf53eb1de724a426ef75106372884ea01bd21e
Krisha8179 <krishnarao1440@gmail.com>
2023-09-20 17:08:45 +0530

    updated

--- util/database.js
+++ util/database.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 
 const sequelize = new Sequelize(`${process.env.MYSQL_SCHEMA}`,`${process.env.MYSQL_USER}`,`${process.env.MYSQL_PASSWORD}`, {
     dialect: 'mysql',
-    host: `${process.env.DB_HOST}`
+    host: `${process.env.DB_HOST}`,
+    port: 3306
 });
 
 module.exports = sequelize;
